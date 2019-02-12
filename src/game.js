0842e61648b6ec52d211fbcff55e28508fd3963c
kenneth-yu <kyu22@buffalo.edu>
2019-02-12 09:47:33 -0500

    player sprite no longer throws errors

--- src/game.js
+++ src/game.js
@@ -13,15 +13,12 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-  this.load.spritesheet('dude', '../images/dude.png', 32, 48);
+  this.load.spritesheet('dude', '../images/dude.png', { frameWidth: 32, frameHeight: 48 });
   this.load.image('background', '../images/background2.png');
-  // this.load.image('gr', 'assets/platform.png')
-  // this.load.image('star', 'assets/star.png');
 }
 
 function create ()
 {
-  // this.add.image(400, 300, 'background');
   bg = this.add.tileSprite(0, 0, 1600, 1200, 'background');
   player = this.add.sprite(32,320,'dude');
 
