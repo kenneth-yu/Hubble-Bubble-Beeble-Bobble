c7a7943dde720696295362c8179e2ea462f996a3
lsamano <leizl.samano@gmail.com>
2019-02-15 10:21:32 -0500

    work

--- hubble/config/routes.rb
+++ hubble/config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :playable_sprites, only: [:index, :create, :update]
       resources :highscores, only: [:index, :create, :update]
-<<<<<<< HEAD
       resources :animations, only: [:index, :show, :create, :update]
-=======
-      resources :animations, only: [:index, :create, :update]
->>>>>>> 14d6cb79119096c4bf4905ffde8ed40ab571d9b8
       resources :ai_sprites, only: [:index, :create, :update]
     end
   end
